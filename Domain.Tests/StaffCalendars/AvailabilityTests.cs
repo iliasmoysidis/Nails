3b837252b0848f9caa989f8bb7e074e78eb9abb9
Ilias <iliamous@iti.gr>
2025-12-21 11:19:53 +0200

    Create tests for StaffCalendar

---
+++ Domain.Tests/StaffCalendars/AvailabilityTests.cs
@@ -0,0 +1,195 @@
+using Domain.Entities;
+using Domain.Exceptions;
+using Domain.ValueObjects.Calendar;
+using Domain.ValueObjects.Time;
+using FluentAssertions;
+
+namespace StaffCalendars;
+
+public class AvailabilityTests
+{
+    [Fact]
+    public void IsProfessionalAvailable_ShouldReturnTrueWhenInsideWorkingHours()
+    {
+        var calendar = StaffCalendar.Create(storeId: 1, professionalId: 1);
+
+        calendar.SetWorkingDay(
+            WorkingDay.WithRanges(
+                DayOfWeek.Monday,
+                new[]
+                {
+                    new TimeRange(TimeSpan.FromHours(9), TimeSpan.FromHours(17))
+                }
+            )
+        );
+
+        var start = UtcDateTime.From(
+            new DateTime(2025, 1, 6, 10, 0, 0, DateTimeKind.Utc)
+        );
+
+        var end = start.AddHours(1);
+
+        calendar.IsProfessionalAvailable(start, end).Should().Be(true);
+    }
+
+    [Fact]
+    public void IsProfessionalAvailable_ShouldReturnTrueWhenInsidePartialException()
+    {
+        var calendar = StaffCalendar.Create(storeId: 1, professionalId: 1);
+
+        calendar.AddException(CalendarException.PartialDay(
+            new DateOnly(2025, 1, 6),
+            new[]
+            {
+                new TimeRange(TimeSpan.FromHours(17), TimeSpan.FromHours(21))
+
+            }
+        )
+        );
+
+        var start = UtcDateTime.From(
+            new DateTime(2025, 1, 6, 18, 0, 0, DateTimeKind.Utc)  // Monday
+        );
+
+        var end = start.AddHours(2);
+
+        calendar.IsProfessionalAvailable(start, end).Should().Be(true);
+    }
+
+    [Fact]
+    public void IsProfessionalAvailable_ShouldReturnFalseWhenOutsideWorkingHours()
+    {
+        var calendar = StaffCalendar.Create(storeId: 1, professionalId: 1);
+
+        calendar.SetWorkingDay(
+            WorkingDay.WithRanges(
+                DayOfWeek.Monday,
+                new[]
+                {
+                    new TimeRange(TimeSpan.FromHours(9), TimeSpan.FromHours(17))
+                }
+            )
+        );
+
+        var start = UtcDateTime.From(
+            new DateTime(2025, 1, 6, 10, 0, 0, DateTimeKind.Utc)
+        );
+
+        var end = start.AddHours(10);
+
+        calendar.IsProfessionalAvailable(start, end).Should().Be(false);
+    }
+
+    [Fact]
+    public void IsProfessionalAvailable_ShouldReturnFalseWhenDayOff()
+    {
+        var calendar = StaffCalendar.Create(storeId: 1, professionalId: 1);
+
+        calendar.SetDayOff(DayOfWeek.Monday);
+
+        var start = UtcDateTime.From(
+            new DateTime(2025, 1, 6, 10, 0, 0, DateTimeKind.Utc)  // Monday
+        );
+
+        var end = start.AddHours(2);
+
+        calendar.IsProfessionalAvailable(start, end).Should().Be(false);
+    }
+
+    [Fact]
+    public void IsProfessionalAvailable_ShouldReturnFalseWhenOutsidePartialException()
+    {
+        var calendar = StaffCalendar.Create(storeId: 1, professionalId: 1);
+
+        calendar.AddException(CalendarException.PartialDay(
+            new DateOnly(2025, 1, 6),
+            new[]
+            {
+                new TimeRange(TimeSpan.FromHours(17), TimeSpan.FromHours(21))
+
+            }
+        )
+        );
+
+        var start = UtcDateTime.From(
+            new DateTime(2025, 1, 6, 10, 0, 0, DateTimeKind.Utc)  // Monday
+        );
+
+        var end = start.AddHours(2);
+
+        calendar.IsProfessionalAvailable(start, end).Should().Be(false);
+    }
+
+    [Fact]
+    public void IsProfessionalAvailable_ShouldReturnFalseWhenExceptionDayOff()
+    {
+        var calendar = StaffCalendar.Create(storeId: 1, professionalId: 1);
+
+        calendar.AddException(CalendarException.DayOff(
+            new DateOnly(2025, 1, 6)
+            )
+        );
+
+        var start = UtcDateTime.From(
+            new DateTime(2025, 1, 6, 10, 0, 0, DateTimeKind.Utc)  // Monday
+        );
+
+        var end = start.AddHours(2);
+
+        calendar.IsProfessionalAvailable(start, end).Should().Be(false);
+    }
+
+    [Fact]
+    public void IsProfessionalAvailable_ShouldReturnFalseWhenEndDateIsDifferentThanStartDate()
+    {
+        var calendar = StaffCalendar.Create(storeId: 1, professionalId: 1);
+
+        calendar.SetWorkingDay(
+            WorkingDay.WithRanges(
+                DayOfWeek.Monday,
+                new[]
+                {
+                    new TimeRange(TimeSpan.FromHours(9), TimeSpan.FromHours(17))
+                }
+            )
+        );
+
+        var start = UtcDateTime.From(
+            new DateTime(2025, 1, 6, 10, 0, 0, DateTimeKind.Utc)
+        );
+
+        var end = start.AddHours(24);
+
+        calendar.IsProfessionalAvailable(start, end).Should().Be(false);
+    }
+
+    [Fact]
+    public void IsProfessionalAvailable_ShouldThrow_WhenEndBeforeStartTime()
+    {
+        var calendar = StaffCalendar.Create(storeId: 1, professionalId: 1);
+
+        calendar.SetWorkingDay(
+            WorkingDay.WithRanges(
+                DayOfWeek.Monday,
+                new[]
+                {
+                    new TimeRange(TimeSpan.FromHours(9), TimeSpan.FromHours(17))
+                }
+            )
+        );
+
+        var start = UtcDateTime.From(
+            new DateTime(2025, 1, 6, 10, 0, 0, DateTimeKind.Utc)
+        );
+
+        var end = start.AddHours(-1);
+
+        Action act = () => calendar.IsProfessionalAvailable(start, end);
+
+        act.Should()
+            .Throw<DomainException>()
+            .WithMessage("End time must be after start time.");
+    }
+
+
+}
