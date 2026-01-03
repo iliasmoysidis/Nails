ba3c76846e299fec4e4a9eb4deda4c258673ec44
Ilias <iliamous@iti.gr>
2026-01-03 20:11:40 +0200

    Rename to ensure consistency

--- Domain.Tests/Entities/Staffs/StaffTests.cs
+++ Domain.Tests/Entities/Staffs/StaffTests.cs
@@ -1,7 +1,8 @@
+using Domain.Entities;
 using Domain.Exceptions;
 using FluentAssertions;
 
-namespace Domain.Tests.Entities.Staff;
+namespace Domain.Tests.Entities.Staffs;
 
 public class StaffTests
 {
@@ -9,7 +10,7 @@ public class StaffTests
     [Fact]
     public void Create_ShouldAddInitialOwner()
     {
-        var staff = Domain.Entities.Staff.Create(storeId: 1, initialOwnerId: 10);
+        var staff = Staff.Create(storeId: 1, initialOwnerId: 10);
 
         staff.IsOwner(10).Should().Be(true);
     }
@@ -17,7 +18,7 @@ public class StaffTests
     [Fact]
     public void EnsureOwner_ShouldNotThrow_WhenProfessionalIsOwner()
     {
-        var staff = Domain.Entities.Staff.Create(storeId: 1, initialOwnerId: 10);
+        var staff = Staff.Create(storeId: 1, initialOwnerId: 10);
 
         staff.Invoking(s => s.EnsureOwner(10)).Should().NotThrow();
     }
@@ -25,7 +26,7 @@ public class StaffTests
     [Fact]
     public void EnsureOwner_ShouldThrow_WhenProfessionalIsNotOwner()
     {
-        var staff = Domain.Entities.Staff.Create(storeId: 1, initialOwnerId: 10);
+        var staff = Staff.Create(storeId: 1, initialOwnerId: 10);
 
         staff.Invoking(s => s.EnsureOwner(9)).Should().Throw<DomainException>().WithMessage("Only an owner can perform this action.");
     }
@@ -33,7 +34,7 @@ public class StaffTests
     [Fact]
     public void EnsureStaff_ShouldNotThrow_WhenProfessionalIsStaff()
     {
-        var staff = Domain.Entities.Staff.Create(storeId: 1, initialOwnerId: 10);
+        var staff = Staff.Create(storeId: 1, initialOwnerId: 10);
 
         staff.AddStaff(10, 11);
 
@@ -43,7 +44,7 @@ public class StaffTests
     [Fact]
     public void EnsureStaff_ShouldThrow_WhenProfessionalIsNotStaff()
     {
-        var staff = Domain.Entities.Staff.Create(storeId: 1, initialOwnerId: 10);
+        var staff = Staff.Create(storeId: 1, initialOwnerId: 10);
 
         staff.Invoking(s => s.EnsureStaff(11)).Should().Throw().WithMessage("Professional does not work for the store.");
     }
@@ -51,7 +52,7 @@ public class StaffTests
     [Fact]
     public void AddOwner_ShouldNotThrow_WhenAddingNewOwner()
     {
-        var staff = Domain.Entities.Staff.Create(storeId: 1, initialOwnerId: 10);
+        var staff = Staff.Create(storeId: 1, initialOwnerId: 10);
 
         staff.Invoking(s => s.AddOwner(10, 11)).Should().NotThrow();
 
@@ -61,7 +62,7 @@ public class StaffTests
     [Fact]
     public void AddOwner_ShouldThrow_WhenProfessionalAlreadyOwner()
     {
-        var staff = Domain.Entities.Staff.Create(storeId: 1, initialOwnerId: 10);
+        var staff = Staff.Create(storeId: 1, initialOwnerId: 10);
 
         staff.Invoking(s => s.AddOwner(10, 10)).Should().Throw<DomainException>().WithMessage("Professional is already an owner of this store.");
     }
@@ -69,7 +70,7 @@ public class StaffTests
     [Fact]
     public void RemoveOwner_ShouldNotThrow_WhenRemovingAnOwner()
     {
-        var staff = Domain.Entities.Staff.Create(storeId: 1, initialOwnerId: 10);
+        var staff = Staff.Create(storeId: 1, initialOwnerId: 10);
 
         staff.AddOwner(10, 11);
 
@@ -81,7 +82,7 @@ public class StaffTests
     [Fact]
     public void RemoveOwner_ShouldThrow_WhenRemovingLastOwner()
     {
-        var staff = Domain.Entities.Staff.Create(storeId: 1, initialOwnerId: 10);
+        var staff = Staff.Create(storeId: 1, initialOwnerId: 10);
 
         staff.Invoking(s => s.RemoveOwner(10, 10)).Should().Throw<DomainException>().WithMessage("Cannot remove last owner.");
     }
@@ -89,7 +90,7 @@ public class StaffTests
     [Fact]
     public void RemoveOwner_ShouldThrow_WhenProfessionalIsNotOwner()
     {
-        var staff = Domain.Entities.Staff.Create(storeId: 1, initialOwnerId: 10);
+        var staff = Staff.Create(storeId: 1, initialOwnerId: 10);
 
         staff.AddOwner(10, 11);
 
@@ -99,7 +100,7 @@ public class StaffTests
     [Fact]
     public void AddStaff_ShouldNotThrow_WhenAddingStaff()
     {
-        var staff = Domain.Entities.Staff.Create(storeId: 1, initialOwnerId: 10);
+        var staff = Staff.Create(storeId: 1, initialOwnerId: 10);
 
         staff.Invoking(s => s.AddStaff(10, 11)).Should().NotThrow();
 
@@ -109,7 +110,7 @@ public class StaffTests
     [Fact]
     public void AddStaff_ShouldThrow_WhenAddingExistingStaff()
     {
-        var staff = Domain.Entities.Staff.Create(storeId: 1, initialOwnerId: 10);
+        var staff = Staff.Create(storeId: 1, initialOwnerId: 10);
 
         staff.AddStaff(10, 11);
 
@@ -119,7 +120,7 @@ public class StaffTests
     [Fact]
     public void RemoveStaff_ShouldNotThrow_WhenRemovingStaff()
     {
-        var staff = Domain.Entities.Staff.Create(storeId: 1, initialOwnerId: 10);
+        var staff = Staff.Create(storeId: 1, initialOwnerId: 10);
 
         staff.AddStaff(10, 11);
 
@@ -131,7 +132,7 @@ public class StaffTests
     [Fact]
     public void RemoveStaff_ShouldThrow_WhenRemovingProfessionalThatIsNotStaff()
     {
-        var staff = Domain.Entities.Staff.Create(storeId: 1, initialOwnerId: 10);
+        var staff = Staff.Create(storeId: 1, initialOwnerId: 10);
 
         staff.Invoking(s => s.RemoveStaff(10, 11)).Should().Throw<DomainException>().WithMessage("Professional does not work for the store.");
     }
