4bc15fd767f4908b31458066cc08871654410eda
Ilias <iliamous@iti.gr>
2026-01-10 19:15:05 +0200

    Clean code

--- Domain/Interfaces/IAvailabilityService.cs
+++
@@ -1,9 +0,0 @@
-using Domain.ValueObjects.Time;
-
-namespace Domain.Interfaces;
-
-public interface IAvailabilityService
-{
-    Task EnsureStoreIsOpenAsync(int storeId, UtcDateTime startAt, UtcDateTime endAt);
-    Task EnsureProfessionalIsAvailableAsync(int storeId, int professionalId, UtcDateTime startAt, UtcDateTime endAt);
-}
