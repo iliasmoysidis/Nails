5dc034447b525127f3a86bdfff5d4f0dcf4e504c
Ilias <iliamous@iti.gr>
2026-01-11 14:17:31 +0200

    Add configuration files for StoreCalendar and StoreCatalog

---
+++ Infrastructure/Persistence/Configurations/StoreCatalogConfig.cs
@@ -0,0 +1,52 @@
+using Domain.Entities;
+using Domain.ValueObjects.Offerings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations;
+
+public sealed class StoreCatalogConfig : IEntityTypeConfiguration<StoreCatalog>
+{
+    public void Configure(EntityTypeBuilder<StoreCatalog> builder)
+    {
+        builder.ToTable("store_catalogs");
+
+        builder.HasKey(c => c.StoreId);
+
+        builder.Property(c => c.StoreId)
+            .ValueGeneratedNever();
+
+        builder.HasMany<Offering>("_offerings")
+            .WithOne()
+            .HasForeignKey(o => o.StoreId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Navigation("_offerings")
+            .UsePropertyAccessMode(PropertyAccessMode.Field);
+
+        builder.OwnsMany<ServiceOffering>("_serviceOfferings", so =>
+        {
+            so.ToTable("service_offerings");
+
+            so.WithOwner()
+             .HasForeignKey("StoreId");
+
+            so.Property<int>("StoreId");
+
+            so.HasKey(
+                "StoreId",
+                nameof(ServiceOffering.ProfessionalId),
+                nameof(ServiceOffering.OfferingId)
+            );
+
+            so.Property(x => x.ProfessionalId)
+                .IsRequired();
+
+            so.Property(x => x.OfferingId)
+                .IsRequired();
+        });
+
+        builder.Navigation("_serviceOfferings")
+            .UsePropertyAccessMode(PropertyAccessMode.Field);
+    }
+}
