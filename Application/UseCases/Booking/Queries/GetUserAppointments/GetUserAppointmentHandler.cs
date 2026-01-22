dfed40da85a1d95afd9582d84192cd134b0ec72a
Ilias Moysidis <iliamous@iti.gr>
2026-01-22 15:35:40 +0200

    Add a condition for GetUserAppointments so that only the user can see his own appointments

--- Application/UseCases/Booking/Queries/GetUserAppointments/GetUserAppointmentHandler.cs
+++ Application/UseCases/Booking/Queries/GetUserAppointments/GetUserAppointmentHandler.cs
@@ -1,5 +1,6 @@
 using Application.Abstractions;
 using Application.DTO;
+using Application.Exceptions;
 using Application.Repositories;
 
 namespace Application.UseCases.Booking.Queries.GetUserAppointments;
@@ -7,14 +8,19 @@ namespace Application.UseCases.Booking.Queries.GetUserAppointments;
 public sealed class GetUserAppointmentsHandler : IQueryHandler<GetUserAppointmentsQuery, IReadOnlyCollection<AppointmentListItemDTO>>
 {
     private readonly IBookingReadRepository _repo;
+    private readonly ICurrentUser _currentUser;
 
-    public GetUserAppointmentsHandler(IBookingReadRepository repo)
+    public GetUserAppointmentsHandler(IBookingReadRepository repo, ICurrentUser currentUser)
     {
         _repo = repo;
+        _currentUser = currentUser;
     }
 
     public async Task<IReadOnlyCollection<AppointmentListItemDTO>> Handle(GetUserAppointmentsQuery query, CancellationToken ct)
     {
+        if (query.UserId != _currentUser.UserId)
+            throw new ApplicationLayerException("You are not allowed to view these appointments.");
+
         return await _repo.GetForUserAsync(query.UserId, ct);
     }
 }
