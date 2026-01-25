56fe4b49342dbc25d77aede7d0544216eeb063e8
Ilias <iliamous@iti.gr>
2026-01-25 13:00:56 +0200

    Refactor Store

--- Domain/Entities/Store.cs
+++ Domain/Entities/Store.cs
@@ -1,4 +1,5 @@
 using Domain.Common;
+using Domain.Exceptions;
 using Domain.Interfaces;
 using Domain.ValueObjects.Identity;
 using Domain.ValueObjects.Store;
@@ -16,19 +17,87 @@ public class Store : HistoricEntity
 
     private Store() { }
 
-    public static Store Create(StoreName name, Address address, TaxIdentificationNumber taxIdNumber, Email email, Phone phone, IClock clock)
+    private Store(
+        StoreName name,
+        Address address,
+        TaxIdentificationNumber taxIdNumber,
+        Email email,
+        Phone phone
+    )
     {
-        var store = new Store
-        {
-            Name = name,
-            Address = address,
-            TaxIdNumber = taxIdNumber,
-            Email = email,
-            Phone = phone
-        };
+        Name = name;
+        Address = address;
+        TaxIdNumber = taxIdNumber;
+        Email = email;
+        Phone = phone;
+    }
+
+    public static Store Create(
+        StoreName name,
+        Address address,
+        TaxIdentificationNumber taxIdNumber,
+        Email email,
+        Phone phone,
+        IClock clock
+        )
+    {
+        var store = new Store(
+            name: name,
+            address: address,
+            taxIdNumber: taxIdNumber,
+            email: email,
+            phone: phone
+        );
+
 
         store.MarkAsCreated(clock);
 
         return store;
+    }
+
+    public void UpdateDetails(
+        IClock clock,
+        StoreName? name = null,
+        Address? address = null,
+        Phone? phone = null
+    )
+    {
+        EnsureNotDeleted();
+
+        var changed = false;
+
+        changed |= TryUpdateName(name);
+        changed |= TryUpdateAddress(address);
+        changed |= TryUpdatePhone(phone);
+
+        if (changed)
+            MarkAsUpdated(clock);
+    }
+
+    private bool TryUpdateName(StoreName? name)
+    {
+        if (name is null || name == Name) return false;
+        Name = name;
+        return true;
+    }
+
+    private bool TryUpdateAddress(Address? address)
+    {
+        if (address is null || address == Address) return false;
+        Address = address;
+        return true;
+    }
+
+    private bool TryUpdatePhone(Phone? phone)
+    {
+        if (phone is null || phone == Phone) return false;
+        Phone = phone;
+        return true;
+    }
+
+    private void EnsureNotDeleted()
+    {
+        if (IsDeleted)
+            throw new DomainException("Store is deleted.");
     }
 }
