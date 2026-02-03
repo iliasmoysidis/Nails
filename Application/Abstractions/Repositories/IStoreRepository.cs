f71a8bfe6d72aa66f919b3c87ccc6b63039cc97a
Ilias Moysidis <iliamous@iti.gr>
2026-02-03 11:48:30 +0200

    Add UpdateDetails use case

--- Application/Abstractions/Repositories/IStoreRepository.cs
+++ Application/Abstractions/Repositories/IStoreRepository.cs
@@ -4,7 +4,7 @@ namespace Application.Abstractions.Repositories;
 
 public interface IStoreRepository
 {
-    Task<StoreCalendar?> GetByStoreIdAsync(int storeId, CancellationToken ct);
+    Task<Store?> GetByStoreIdAsync(int storeId, CancellationToken ct);
 
     Task AddAsync(Store store, CancellationToken ct);
 }
---
+++ Application/Commands/Stores/UpdateDetails/UpdateDetailsHandler.cs
@@ -0,0 +1,56 @@
+using Application.Abstractions.Repositories;
+using Application.Abstractions.UnitOfWork;
+using Application.Exceptions;
+using Domain.Interfaces;
+using Domain.ValueObjects.Identity;
+using Domain.ValueObjects.Store;
+
+namespace Application.Commands.Stores;
+
+public sealed class UpdateDetailsHandler
+{
+    private readonly IStoreRepository _repo;
+    private readonly IClock _clock;
+    private readonly IUnitOfWork _uow;
+
+    public UpdateDetailsHandler(
+        IStoreRepository repo,
+        IClock clock,
+        IUnitOfWork uow
+    )
+    {
+        _repo = repo;
+        _clock = clock;
+        _uow = uow;
+    }
+
+    public async Task Handle(UpdateDetailsCommand command, CancellationToken ct)
+    {
+        var store = await _repo.GetByStoreIdAsync(command.StoreId, ct)
+            ?? throw new ApplicationLayerNotFoundException("Store not found.");
+
+        store.UpdateDetails(
+            clock: _clock,
+            name: command.Name is null
+                ? null
+                : StoreName.Create(command.Name),
+            address: command.Street is null
+                ? null
+                : Address.From(
+                    street: command.Street,
+                    city: command.City!,
+                    postalCode: command.PostalCode!,
+                    state: command.State!,
+                    countryCode: command.CountryCode!
+                ),
+            phone: command.PhoneCountryCode is null
+                ? null
+                : Phone.From(
+                    countryCode: command.PhoneCountryCode,
+                    nationalNumber: command.PhoneNumber!
+                )
+        );
+
+        await _uow.SaveChangesAsync(ct);
+    }
+}
