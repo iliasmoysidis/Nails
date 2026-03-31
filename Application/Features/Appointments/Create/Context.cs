a764a72baaacf24a7d0238ff6ecb84ae9001210f
Ilias <iliamous@iti.gr>
2026-04-01 00:28:16 +0300

    Fix assignments in features

--- Application/Features/Appointments/Create/Context.cs
+++ Application/Features/Appointments/Create/Context.cs
@@ -8,6 +8,6 @@ public sealed class Context
     public Offering Offering { get; set; } = default!;
     public StoreCalendar StoreCalendar { get; set; } = default!;
     public StaffCalendar StaffCalendar { get; set; } = default!;
-    public Assignments Assignments { get; set; } = default!;
+    public Domain.Entities.Assignments Assignments { get; set; } = default!;
     public IReadOnlyCollection<Appointment> Appointments { get; set; } = default!;
 }
